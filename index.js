fe73d15f7e221a6578a73f69b994df4913f4dfe7
Leonardo Alves(leo-pro) <leoroberto.alves@hotmail.com>
2021-01-29 01:23:10 -0300

    Submit Challenge v1.0

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const CSVToJSON = require('csvtojson');
 const fs = require('fs');
 
-function readCSV(fileInput, fileOutput){
+function convertCsvToJson(fileInput, fileOutput){
   CSVToJSON().fromFile(fileInput)
   .then(data => {
-      json = JSON.stringify(data); 
-      fs.writeFileSync(fileOutput, json); 
-      console.log('CSV Converted with success');
+    data.forEach((item) => {
+      item['group'] = item['group'].split(',') || item.group.split('/');
+      item['email Pedagogical Responsible'] = item['email Pedagogical Responsible'].split('/');
+    });
+    
+    const json = JSON.stringify(data); 
+    fs.writeFileSync(fileOutput, json); 
+    console.log('CSV Converted with success');
+
   }).catch(err => {
       console.log(err);
   });
-}
+} 
 
-readCSV('input.csv','output.json');
+convertCsvToJson('input.csv','output.json');
